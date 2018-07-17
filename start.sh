97860db93339e6ba6f972feda1d5a5b9eea32419
Diego Morales <diego.morales@gmx.net>
2018-07-18 00:53:20 +0200

    Update start script

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ if [[ "$(docker images -q $IMG_NAME 2> /dev/null)" == "" ]]; then
   docker build -t $IMG_NAME .
 fi
 
-docker run -it -p 3000:3000 -p 3001:3001 -v $(pwd):/home/node/code $IMG_NAME bash
+docker run -it -v $(pwd):/home/node/code $IMG_NAME bash
