69068ba47de5758a4bec01dc876e1bf725885287
Diego Morales <diego.morales@gmx.net>
2018-07-19 14:55:21 +0200

    add test

--- src/round.js
+++ src/round.js
@@ -5,7 +5,7 @@
  * @desc Rounds a number to given decimal places.
  *
  * @param {number} number - Number to round
- * @param {number} decimals=2 - Decimal places
+ * @param {number} decimals=0 - Decimal places
  * @returns {number} Rounded number
  */
-export const round = (number, decimals = 2) => Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals)
+export const round = (number, decimals = 0) => Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals)
