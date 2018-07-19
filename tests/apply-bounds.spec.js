e15dfb4aedb72ded443d6599cf47062770124310
Diego Morales <diego.morales@gmx.net>
2018-07-20 00:00:17 +0200

    add test

---
+++ tests/apply-bounds.spec.js
@@ -0,0 +1,10 @@
+let test = require('tape')
+let applyBounds = require('../').applyBounds
+
+test('"applyBounds" method', (t) => {
+  t.plan(3)
+
+  t.equal(applyBounds(5, 0, 10), 5, 'Value is unchanged if it\'s in bounds')
+  t.equal(applyBounds(100, 0, 10), 10, 'Value equals upper bound if value is greater than upper bound')
+  t.equal(applyBounds(-100, 0, 10), 0, 'Value equals lower bound if value is less than lower bound')
+})
