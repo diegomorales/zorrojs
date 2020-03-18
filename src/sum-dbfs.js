78a27a6481df1bcf0d0644867584a7a444ef8b40
Diego Morales <diego.morales@gmx.net>
2020-03-18 15:14:36 +0100

    fix doc

--- src/sum-dbfs.js
+++ src/sum-dbfs.js
@@ -4,9 +4,9 @@
  *
  * @desc Sums two dBFS values
  *
- * @param {number} dB1 dBFS value
- * @param {number} dB2 dBFS value
+ * @param {number} dB1 - dBFS value
+ * @param {number} dB2 - dBFS value
  *
  * @returns {number} Resulting dBFS value
  */
-export const sumDBFS = (db1, db2) => 20 * (Math.log(Math.pow(10, (db1 / 20)) + Math.pow(10, (db2 / 20)))) / Math.log(10)
+export const sumDBFS = (dB1, dB2) => 20 * (Math.log(Math.pow(10, (dB1 / 20)) + Math.pow(10, (dB2 / 20)))) / Math.log(10)
