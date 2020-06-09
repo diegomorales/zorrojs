3393dc3b433b1cb8bf5f5d925c9d2e174fd1ceb1
Diego Morales <diego.morales@gmx.net>
2020-06-09 14:00:21 +0200

    reorder

--- index.js
+++ index.js
@@ -4,60 +4,60 @@
  *  @module zorrojs
  * */
 
-import { toDBFS } from './src/to-dbfs'
-import { sumDBFS } from './src/sum-dbfs'
-import { round } from './src/round'
-import { matrix2Deg } from './src/matrix-2-deg'
-import { deg2Matrix } from './src/deg-2-matrix'
-import { matrix2xy } from './src/matrix-2-xy'
-import { translateValue } from './src/translate-value'
-import { applyBounds } from './src/apply-bounds'
-import { getStyleValue } from './src/get-style-value'
-import { once } from './src/once'
-import { copyObject } from './src/copy-object'
-import { valueBuilder } from './src/value-builder'
-import { random } from './src/random'
-import { last } from './src/last'
-import { deg2Rad } from './src/deg-2-rad'
-import { shuffle } from './src/shuffle'
-import { compose } from './src/compose'
-import { snapValue } from './src/snap-value'
-import { arc } from './src/arc'
 import { animateProps } from './src/animate-props'
-import * as easings from './src/easings'
-import { setStyles } from './src/set-styles'
+import { applyBounds } from './src/apply-bounds'
+import { arc } from './src/arc'
 import { clearStyles } from './src/clear-styles'
-import { str2dom } from './src/str-2-dom'
-import { merge } from './src/merge'
-import { completeAssign } from './src/complete-assign'
 import { compareArrays } from './src/compare-arrays'
+import { completeAssign } from './src/complete-assign'
+import { compose } from './src/compose'
+import { copyObject } from './src/copy-object'
+import { deg2Matrix } from './src/deg-2-matrix'
+import { deg2Rad } from './src/deg-2-rad'
+import * as easings from './src/easings'
+import { getStyleValue } from './src/get-style-value'
+import { last } from './src/last'
+import { matrix2Deg } from './src/matrix-2-deg'
+import { matrix2xy } from './src/matrix-2-xy'
+import { merge } from './src/merge'
+import { once } from './src/once'
+import { random } from './src/random'
+import { round } from './src/round'
+import { setStyles } from './src/set-styles'
+import { shuffle } from './src/shuffle'
+import { snapValue } from './src/snap-value'
+import { str2dom } from './src/str-2-dom'
+import { sumDBFS } from './src/sum-dbfs'
+import { toDBFS } from './src/to-dbfs'
+import { translateValue } from './src/translate-value'
+import { valueBuilder } from './src/value-builder'
 
 export {
-  toDBFS,
-  sumDBFS,
-  round,
-  matrix2Deg,
-  deg2Matrix,
-  matrix2xy,
-  translateValue,
-  applyBounds,
-  getStyleValue,
-  once,
-  copyObject,
-  valueBuilder,
-  random,
-  last,
-  deg2Rad,
-  shuffle,
-  compose,
-  snapValue,
-  arc,
   animateProps,
-  easings,
-  setStyles,
+  applyBounds,
+  arc,
   clearStyles,
-  str2dom,
-  merge,
+  compareArrays,
   completeAssign,
-  compareArrays
+  compose,
+  copyObject,
+  deg2Matrix,
+  deg2Rad,
+  easings,
+  getStyleValue,
+  last,
+  matrix2xy,
+  matrix2Deg,
+  merge,
+  once,
+  random,
+  round,
+  setStyles,
+  shuffle,
+  snapValue,
+  str2dom,
+  sumDBFS,
+  toDBFS,
+  translateValue,
+  valueBuilder
 }
